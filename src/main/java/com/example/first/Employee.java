80b241557db04c8645d7993bc319267cf7656380
YadavShashank36 <shashankyadavrocks@gmail.com>
2024-10-05 18:16:02 +0530

    initial commit

---
+++ src/main/java/com/example/first/Employee.java
@@ -0,0 +1,43 @@
+package com.example.first;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+ @Id
+ @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastName;
+public Employee() {}
+public Employee(Long id, String firstName, String lastName) {
+	super();
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+}
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+  
+}
+	
+}
