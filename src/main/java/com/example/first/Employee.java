faec9cb77cd26e07e8b0b11b27a662d4981cbbbb
YadavShashank36 <shashankyadavrocks@gmail.com>
2024-10-08 00:37:19 +0530

    first Spring non rest completed

--- src/main/java/com/example/first/Employee.java
+++ src/main/java/com/example/first/Employee.java
@@ -11,33 +11,37 @@ public class Employee {
  @Id
  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
-  private String firstName;
-  private String lastName;
+  private String name;
+  private String role;
 public Employee() {}
-public Employee(Long id, String firstName, String lastName) {
+
+public Employee(Long id, String name, String role) {
 	super();
 	this.id = id;
-	this.firstName = firstName;
-	this.lastName = lastName;
+	this.name = name;
+	this.role = role;
 }
+
+
 public Long getId() {
 	return id;
 }
 public void setId(Long id) {
 	this.id = id;
 }
-public String getFirstName() {
-	return firstName;
+public String getName() {
+	return name;
 }
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
+public void setName(String name) {
+	this.name = name;
 }
-public String getLastName() {
-	return lastName;
+public String getRole() {
+	return role;
 }
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-  
+
+public void setRole(String role) {
+	this.role = role;
 }
+ 
 	
 }
--- src/main/java/com/example/first/EmployeeController.java
+++ src/main/java/com/example/first/EmployeeController.java
@@ -2,7 +2,12 @@ package com.example.first;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +23,37 @@ public class EmployeeController {
 	   System.out.println(repository.count());
 	    return repository.findAll();
    }
+   
+   @PostMapping("/employees")
+   Employee newEmployee(@RequestBody Employee newEmployee)
+   {
+	  return  repository.save(newEmployee);
+   }
+   
+   @GetMapping("/employees/{id}")
+   Employee getEmployee(@PathVariable Long id)
+   {
+	   return repository.findById(id)
+			   .orElseThrow(()-> new EmployeeNotFoundException(id));
+   }
+   
+   @PutMapping("/employees/{id}")
+   Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id)
+   {
+	 return repository.findById(id)
+			 .map(employee -> {
+				  employee.setName(newEmployee.getName());
+				  employee.setRole(newEmployee.getRole());
+				  return repository.save(employee);
+			 })
+			 .orElseGet(()-> {return  repository.save(newEmployee);});
+   }
+   @DeleteMapping("/employees/{id}")
+   void remove(@PathVariable Long id)
+   {
+	   repository.deleteById(id);
+   }
+   
+   
+   
 }
